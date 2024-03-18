a12ef901be6ed6f73d668db410906991a0bfd332
jonny <js90@hml.com>
2024-03-18 16:24:35 +0200

    remove footer

--- src/layout/footer/Footer.component.tsx
+++ src/layout/footer/Footer.component.tsx
@@ -2,8 +2,8 @@ import { FC } from "react";
 
 const CFooter: FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1>footer</h1>
+    <div className="flex flex-col items-center justify-center">
+      {/* <h1>footer</h1> */}
     </div>
   );
 };
